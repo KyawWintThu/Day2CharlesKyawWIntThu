93bc234736be12994de9b3a08ff654432e4fd0a3
Jeremiahtws <jeremiahtws@gmail.com>
2017-08-17 09:12:57 +0800

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  citylist:string[]=[];
+  city:string = "";
+
+
+  appmethod(stringvalue)
+  { 
+    console.log(stringvalue);
+    for(let i =0; i < this.citylist.length; i++){
+        if(this.citylist[i]==stringvalue)
+          {
+            return;
+          }
+      }
+    this.citylist.push(stringvalue);
+  }
+
+  getCity(value){
+    console.log(value);
+    this.city = value;
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AddcityComponent } from './component/addcity.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddcityComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
